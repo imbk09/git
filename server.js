902bd20f5b5f7cc2b7a2c2361e29db1465ecdf92
bharat <bharat@localhost.localdomain>
2025-04-09 17:58:25 -0600

    jairo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:jairo');
+  res.send('CPSY 350 Project: Github Actions CI. SAIT ID:jairo1');
 });
 
 const server = app.listen(PORT, HOST);
